bbb9f179ddad6511c557faf5fc0b2c0549d6338d
SC <github.com@genkisugi.net>
2015-07-28 22:45:14 -0700

    bump docs

--- doc/scripts/jsdoc-toc.js
+++ doc/scripts/jsdoc-toc.js
@@ -6,7 +6,7 @@
     treeNode.tree({
         autoEscape: false,
         closedIcon: '&#x21e2;',
-        data: [{"label":"<a href=\"module-bbop-rest-response.html\">bbop-rest-response</a>","id":"module:bbop-rest-response","children":[{"label":"<a href=\"module-bbop-rest-response-response.html\">response</a>","id":"module:bbop-rest-response~response","children":[]}]}],
+        data: [{"label":"<a href=\"module-bbop-rest-response.html\">bbop-rest-response</a>","id":"module:bbop-rest-response","children":[{"label":"<a href=\"module-bbop-rest-response-response.html\">response</a>","id":"module:bbop-rest-response~response","children":[]},{"label":"<a href=\"module-bbop-rest-response-response_json.html\">response_json</a>","id":"module:bbop-rest-response~response_json","children":[]}]}],
         openedIcon: ' &#x21e3;',
         saveState: true,
         useContextMenu: false
